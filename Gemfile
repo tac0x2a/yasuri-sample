8e8f3ed188809fe35730e24893bff60796b8c2d2
TAC <tac@tac42.net>
2015-02-23 15:46:02 +0000

    Implement yasuri sample

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'yasuri'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
--- app.rb
+++ app.rb
@@ -5,9 +5,50 @@ require 'sinatra/reloader'
 
 require 'slim'
 
+require 'mechanize'
+require 'json'
+require 'yasuri'
+
+set :server, 'webrick'
+
 before do
+  @url  = params[:url]  || "https://news.ycombinator.com/"
+  @tree = params[:tree] || %Q|
+{
+  "node": "struct",
+  "name": "titles",
+  "path": "//td[@class='title'][not(@align)]",
+  "children": [
+    {
+      "node": "text",
+      "name": "title",
+      "path": "./a"
+    },
+    {
+      "node": "text",
+      "name": "url",
+      "path": "./a/@href"
+    }
+  ]
+}
+|
+  @result = nil
+  @tree = JSON.pretty_generate(JSON.load(@tree))
 end
 
 get '/' do
+  slim :index
+end
+
+post '/' do
+  begin
+    tree  = Yasuri.json2tree(@tree)
+    agent = Mechanize.new
+    @page = agent.get(@url)
+    @result = JSON.pretty_generate(tree.inject(agent, @page))
+  rescue => e
+    @exp = e.to_s
+  end
+
   slim :index
 end
